1d6d13c0ea3549528a0b879ae889714627b29b82
Carlos Fernández <sirikon2@gmail.com>
2017-05-31 20:04:01 +0200

    Some changes to index.js

--- index.js
+++ index.js
@@ -3,37 +3,56 @@
 const fs = require('fs')
 const Twitter = require('twitter')
 const SlackClient = require('@slack/client')
+const http = require('http')
 const SlackWebClient = SlackClient.WebClient
+
+const hostname = '0.0.0.0';
+const port = 8989;
 
 var configRaw = fs.readFileSync('./config.json')
 var config = JSON.parse(configRaw)
 
 var client = new Twitter({
-  consumer_key: config.Twitter.ConsumerKey,
-  consumer_secret: config.Twitter.ConsumerSecret,
-  access_token_key: config.Twitter.AccessToken,
-  access_token_secret: config.Twitter.AccessTokenSecret
+    consumer_key: config.Twitter.ConsumerKey,
+    consumer_secret: config.Twitter.ConsumerSecret,
+    access_token_key: config.Twitter.AccessToken,
+    access_token_secret: config.Twitter.AccessTokenSecret
 })
 
-client.stream('statuses/filter', { follow: config.Twitter.UserIDs.Pixel_Dailies },  function(stream) {
-    console.log('Created stream')
-    stream.on('data', function(tweet) {
-        console.log('Received tweet')
-        var normalizedTwit = tweet.text.toLowerCase().trim()
-        if (normalizedTwit.indexOf('today') === 0) {
-            console.log('Twit is valid, publishing.')
-            var web = new SlackWebClient(config.Slack.APIToken)
-            web.chat.postMessage('art', 'Pixel_Dailies: ' + tweet.text, function(err, res) {
-                if (err) {
-                    console.log('Error:', err);
-                } else {
-                    console.log('Message sent: ', res);
-                }
-            })
-        }
-    })
+function listenStream() {
+    client.stream('statuses/filter', { follow: config.Twitter.UserIDs.Pixel_Dailies },  function(stream) {
+        console.log('Created stream')
+        stream.on('data', function(tweet) {
+            console.log('Received tweet')
+            var normalizedTwit = tweet.text.toLowerCase().trim()
+            if (normalizedTwit.indexOf('today') === 0) {
+                console.log('Twit is valid, publishing.')
+                var web = new SlackWebClient(config.Slack.APIToken)
+                web.chat.postMessage('art', 'Pixel_Dailies: ' + tweet.text, function(err, res) {
+                    if (err) {
+                        console.log('Error:', err);
+                    } else {
+                        console.log('Message sent: ', res);
+                    }
+                })
+            }
+        })
 
-    stream.on('error', function(error) {
-        console.log(error)
+        stream.on('error', function(error) {
+            console.log(error)
+            listenStream()
+        })
     })
+}
+
+listenStream()
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World\n')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
 })

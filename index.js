5abdf94b4ee5b31d426320826f616f07a2682df2
Carlos Fernández <sirikon2@gmail.com>
2017-06-03 16:07:31 +0200

    New slack-based login system

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 'use strict'
 
-const http = require('http')
-const moment = require('moment')
-
-const logger = require('./src/internals/logger')
+const controlPanel = require('./src/controlPanel/index')
 const runPlugin = require('./src/internals/runPlugin')
 const pixelDailiesPlugin = require('./src/plugins/pixelDailies')
 
 runPlugin(pixelDailiesPlugin)
 
-const hostname = '0.0.0.0';
-const port = 8989;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    var state = logger.getState()
-    var log = logger.getLogHistory().map((entry) => {
-        return `[${moment(entry.date).format('YYYY-MM-DD HH:mm:ss')}] ${entry.message}`
-    }).join('\n')
-    res.end(`Restarts: ${state.restartCounter}\nLast ping: ${moment(state.lastPing).format('YYYY-MM-DD HH:mm:ss')}\n\nLog:\n` + log)
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+controlPanel()
--- src/services/slack.js
+++ src/services/slack.js
@@ -6,26 +6,108 @@ const config = require('../internals/config')
 
 /**
  * Provides functionality to interact with the connected Slack.
+ * @constructor
  */
-class SlackService {
-    _createWebClient() {
-        return new SlackClient.WebClient(config.Slack.APIToken)
+function SlackService () {
+    const _createWebClient = () => new SlackClient.WebClient(config.Slack.APIToken)
+
+    /**
+     * Get the user's ID given the Username
+     * @param {string} username - The username
+     * @returns {Promise}
+     * @inner
+     */
+    function _getUserIdByUsername(_username) {
+        var username = _username.replace('@', '')
+        return new Promise((resolve, reject) => {
+            var web = _createWebClient()
+            web.users.list((err, res) => {
+                if (err || !res.ok) return reject(err || new Error('Couldnt get user list'))
+                var found = null
+                res.members.forEach((user) => {
+                    if (user.name === username) {
+                        found = user
+                    }
+                })
+                if (found) {
+                    resolve(found.id)
+                } else {
+                    reject(new Error('Couldnt find username'))
+                }
+            })
+        })
+    }
+
+    /**
+     * Get the direct message channel of the specified user
+     * @param {string} userId - The user ID
+     * @returns {Promise}
+     * @inner
+     */
+    function _getUserDirectMessageChannel(userId) {
+        return new Promise((resolve, reject) => {
+            var web = _createWebClient()
+            web.im.list((err, res) => {
+                if (err || !res.ok) return reject(err || new Error('Couldnt get im list'))
+                var found = null
+                res.ims.forEach((channel) => {
+                    if (channel.user === userId) {
+                        found = channel
+                    }
+                })
+                if (found) {
+                    resolve(found.id)
+                } else {
+                    reject(new Error('Couldnt im channel'))
+                }
+            })
+        })
     }
 
     /**
      * Sends a message to the specified channel
-     * @param {string} channel - The channel name.
+     * @param {string} channel - The channel (public, private or direct message channel).
      * @param {string} message - The message to send.
+     * @returns {Promise}
+     * @inner
      */
-    sendMessage(channel, message) {
+    function _sendMessageToChannel(channel, message) {
         return new Promise((resolve, reject) => {
-            var web = this._createWebClient()
+            var web = _createWebClient()
             web.chat.postMessage(channel, message, { as_user: true },(err, res) => {
                 if (err) return reject(err)
                 resolve()
             })
         })
     }
+
+    /**
+     * Sends a message to the specified username
+     * @param {string} username - The username (can start with @ or not).
+     * @param {string} message - The message to send.
+     * @return {Promise}
+     * @inner
+     */
+    function _sendMessageToUser(username, message) {
+        return _getUserIdByUsername(username)
+            .then(_getUserDirectMessageChannel)
+            .then((userId) => _sendMessageToChannel(userId, message))
+    }
+
+    /**
+     * Sends a message to the specified channel
+     * @param {string} channel - The channel name OR user (starting with @).
+     * @param {string} message - The message to send.
+     * @return {Promise}
+     * @memberof SlackService
+     */
+    function sendMessage(channel, message) {
+        return channel.indexOf('@') === 0
+            ? _sendMessageToUser(channel, message)
+            : _sendMessageToChannel(channel, message)
+    }
+
+    return { sendMessage }
 }
 
-module.exports = () => new SlackService()
+module.exports = () => SlackService()
